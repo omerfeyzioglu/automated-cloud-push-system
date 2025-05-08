b00d011b31da18f6126563da2c7efb388fced7ce
Ömer Feyzioğlu <oomerfeyzioglu@gmail.com>
2025-05-08 10:13:06 +0300

    test

--- src/main/java/com/findata/devopsworkflowpush/DevopsWorkflowPushApplication.java
+++ src/main/java/com/findata/devopsworkflowpush/DevopsWorkflowPushApplication.java
@@ -9,5 +9,5 @@ public class DevopsWorkflowPushApplication {
     public static void main(String[] args) {
         SpringApplication.run(DevopsWorkflowPushApplication.class, args);
     }
-
+//testttt
 }
